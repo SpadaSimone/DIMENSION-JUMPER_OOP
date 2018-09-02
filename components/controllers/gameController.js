c50c9fd2654d6206b19768d14690dbc225c8777c
SpadaSimone <s.spada94@gmail.com>
2018-09-02 19:04:41 +0200

    all features update

---
+++ components/controllers/gameController.js
@@ -0,0 +1,142 @@
+var gameController = angular.module("gameController", []);
+gameController.controller("gameCtrl", ['$scope', '$timeout', '$window', function($scope, $timeout, $window) {
+
+  $scope.blackHoleEventManager = function(i, bHGameMode){
+    var blackHoleDestination = $scope.player1.position + i;
+    var blackHoleDestinationInverted = $scope.player1.position - i;
+    if(bHGameMode === 0 && $scope.nowplayfield.platforms[blackHoleDestination].platform_BlackHole.state === 0){
+      $scope.sharedBlackHoleEvent = $scope.nowplayfield.platforms[blackHoleDestination].platform_BlackHole.destabilize();
+      console.log($scope.nowplayfield.platforms[blackHoleDestination].platform_BlackHole.state);
+      if($scope.nowplayfield.platforms[blackHoleDestination].platform_BlackHole.state === 1){
+        $timeout( function(){
+          $scope.nowplayfield.platforms[blackHoleDestination].platform_BlackHole.stabilize();
+        }, 6000);
+      };
+    } else if (blackHoleDestinationInverted >= 0 && bHGameMode === 1 && $scope.nowplayfield.platforms[blackHoleDestinationInverted].platform_BlackHole.state === 0) {
+      $scope.sharedBlackHoleEvent = $scope.nowplayfield.platforms[blackHoleDestinationInverted].platform_BlackHole.destabilize();
+      console.log($scope.nowplayfield.platforms[blackHoleDestinationInverted].platform_BlackHole.state);
+      if($scope.nowplayfield.platforms[blackHoleDestinationInverted].platform_BlackHole.state === 1){
+        $timeout( function(){
+          $scope.nowplayfield.platforms[blackHoleDestinationInverted].platform_BlackHole.stabilize();
+        }, 6000);
+      };
+    };
+  };
+
+  $scope.holyJump = function(){
+    $scope.player1.holyjump($scope.nowplayfield, $scope.nowDestination);
+  };
+
+  $scope.jumpAndCheck = function(destination){
+    var blackHoleGameMode = $scope.nowplayfield.gameMode;
+    destination = parseInt(destination, );
+    $scope.nowDestination = destination;
+    $scope.blackHoleEventManager(destination, blackHoleGameMode);
+    $timeout(function() {
+      if($scope.player1.state === 0)
+        $scope.player1.jump($scope.nowplayfield.gameMode, destination);
+      $scope.sharedBlackHoleEvent = 0;
+      $scope.distance = 0;
+      $scope.player1.ready();
+    }, 2000);
+    $timeout(function() {
+      if($scope.player1.position === $scope.nowplayfield.platforms.length - 1 && $scope.nowplayfield.platforms[$scope.player1.position].platform_BlackHole.state != 1){
+        $window.location.href = 'winner.html';
+      }else if($scope.player1.position < 0 || $scope.player1.position >= $scope.nowplayfield.platforms.length || $scope.nowplayfield.platforms[$scope.player1.position].platform_BlackHole.state === 1){
+        $window.location.href = 'loser.html';
+      }
+    }, 2300);
+  };
+
+  $scope.inverted_Dimension_Travel = function(){
+    var allClass = document.getElementById('all').getAttribute("class");
+    var all = document.getElementById('all');
+    if($scope.nowplayfield.gameMode != 0){
+      all.classList.add('inverted');
+    }
+    else {
+      all.classList.remove('inverted');
+    }
+  };
+
+  var min = 16;
+  var max = 26;
+
+  $scope.nowDestination = 0;
+  $scope.stopCounter = 0;
+  $scope.sharedBlackHoleEvent = 0;
+  $scope.nowplayfield = new playField;
+  $scope.nowplayfield.createplayfield(min, max);
+  $scope.nowplayfield.playfieldconfirmer(min, max);
+  $scope.player1 = new player;
+  $scope.distance = 0;
+
+  document.getElementById('jform').addEventListener("submit", function(evt){        //cooldown for submit, let the animation run
+    if(document.getElementById('player')){
+      if(document.getElementById('player').getAttribute("class") === 'player ng-scope shake')
+        document.getElementById('player').classList.remove('shake');
+      document.getElementById('player').classList.add('action');
+      window.setTimeout(function(){
+        if(document.getElementById('player') && document.getElementById('player').getAttribute("class") === 'player ng-scope action')
+          document.getElementById('player').classList.remove('action');
+      }, 2200);
+    };
+    document.getElementById('stopb').removeAttribute("disabled");        //sblocca il bottone stop
+    document.getElementById('sj').setAttribute("disabled","disabled");        //blocca il tasto submit jump
+    setTimeout(function(){
+      document.getElementById('stopb').setAttribute("disabled","disabled");
+    }, 1000);
+    setTimeout(function(){
+      document.getElementById('sj').removeAttribute("disabled");
+    }, 2300);
+  }, false);
+
+  document.addEventListener('keydown', (event) => {
+      const keyName = event.key;
+      if(keyName === 's'){
+        document.getElementById('stopb').click();
+        document.getElementById('hj').setAttribute("disabled","disabled");
+      }
+      if(keyName === 'h'){
+        document.getElementById('hj').click();
+        document.getElementById('hj').setAttribute("disabled","disabled");
+      }
+  });
+
+  $scope.$watch("player1.state", function(n){
+    if (n == undefined) {
+      return;
+    };
+    if(n === 1){
+      document.getElementById('player').classList.remove('action');
+      document.getElementById('player').classList.add('shake');
+      $scope.stopCounter = $scope.stopCounter + 1;
+      window.setTimeout(function(){
+        if($scope.stopCounter === 1 && document.getElementById('player').getAttribute("class") === 'player ng-scope shake'){
+          document.getElementById('player').classList.remove('shake');
+        };
+        $scope.stopCounter = $scope.stopCounter - 1;
+      }, 6000);
+    };
+  });
+
+  $scope.$watch("nowplayfield.gameMode", function(n){
+    if (n == undefined) {
+      return;
+    };
+    $scope.inverted_Dimension_Travel();
+  });
+
+  $scope.$watch("sharedBlackHoleEvent", function(n){
+      if (n === undefined) {
+        return;
+      };
+      if(n === 1 && $scope.player1.holyjumps > 0){
+        document.getElementById('hj').removeAttribute("disabled");          //abilita holyjump
+      } else {
+        document.getElementById('hj').setAttribute("disabled","disabled");
+      };
+    }
+  );
+
+}]);
---
+++ components/models/game_models.js
@@ -0,0 +1,168 @@
+class player {
+  constructor(){
+    this.position = 0;
+    this.holyjumps = 3;
+    this.state = 0;
+  };
+
+  jump(gameMode,holuRange){
+    this.state = 0;
+    if(gameMode === 0)
+      this.position = this.position +holuRange;
+    else
+      this.position = this.position -holuRange;
+  };
+
+  stop(){
+    this.state = 1;
+  };
+
+  ready(){
+    this.state = 0;
+  }
+
+  holyjump(playfield, holyRange){
+    this.holyjumps = this.holyjumps - 1;
+    if(playfield.gameMode === 0){
+      var holyDestination = this.position + holyRange;
+    } else {
+      var holyDestination = this.position - holyRange;
+    };
+    playfield.platforms[holyDestination].platform_BlackHole.state = 0;
+  };
+};
+
+
+class BlackHole {
+  constructor(){
+    this.state = 0;
+  };
+
+  destabilize(){
+    var blackHoleProbability = Math.random() < 0.4;
+    if(blackHoleProbability === true){
+      this.state = 1;
+      return (this.state);
+    };
+  };
+
+  stabilize(){
+    this.state = 0;
+  };
+};
+
+class platform {
+  constructor(){
+    this.min_jump;
+    this.max_jump;
+    this.checkmin = false;
+    this.checkmax = false;
+    this.state = 0;
+    this.platform_BlackHole = {};
+  };
+};
+
+class playField {
+  constructor(){
+    this.platforms = [];
+    this.gameMode = 0;
+  };
+
+  randomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  createplayfield(min, max) {          //createplayfield crea il terreno di gioco e posiziona il player sulla prima piattaforma
+    var l = this.randomRange(min, max);
+    this.platforms = [];
+    for (var i = 0; i < l; i++) {
+      var nowplatform = new platform;
+      if (i < l - 1) {
+          nowplatform.min_jump = this.randomRange(1, 5);
+          nowplatform.checkmin = false;
+          nowplatform.checkmax = false;
+          nowplatform.state = 0;
+          nowplatform.platform_BlackHole = new BlackHole;
+          nowplatform.max_jump = this.randomRange(nowplatform.min_jump, 5);
+          if (nowplatform.min_jump != nowplatform.max_jump) {
+            this.platforms.push(nowplatform);       //console.log(nowplatform);
+          } else {
+            nowplatform = null;
+            i--;
+          }
+      } else {
+        nowplatform.state = 0;
+        nowplatform.platform_BlackHole = new BlackHole;
+        this.platforms.push(nowplatform);       //console.log(nowplatform);
+      };
+    }
+    console.log(this);
+  };
+
+  scrollplatforms(platid) {       //scorrimento ricorsivo piattaforme alla ricerca di un percorso vincente
+    var nextplatid;
+    if (platid <= this.platforms.length - 1) {
+      var i = this.platforms[platid].min_jump;
+      nextplatid = platid + i;      //selezione piattaforma successiva in base al percorso selezionato dal ciclo
+      if (platid === this.platforms.length - 1) {        //percorso valido trovato
+        return true;
+      } else {
+        if (platid < this.platforms.length) {
+          return this.scrollplatforms(nextplatid);         //passa alla successiva tappa del percorso
+        }
+      }
+      return false;
+    } else
+      return false;
+  };
+
+  scout(platid, check) {
+    var nextplatid;
+    if (platid < this.platforms.length) {        // ciclo per esplorare tutte le opzioni della piattaforma
+      var i = this.platforms[platid].min_jump;
+      nextplatid = platid + i;
+      if(!this.platforms[platid].checkmin){
+        check = this.scrollplatforms(nextplatid) || check;
+        this.platforms[platid].checkmin = true;
+        i = this.platforms[platid].max_jump;
+      }
+      if(!this.platforms[platid].checkmax){
+        nextplatid = platid + i;
+        check = this.scrollplatforms(nextplatid) || check;
+        this.platforms[platid].checkmax = true;
+      }
+
+      if (check) {
+        return true ;
+      } else {        // ricorsioni per esplorare tutte le opzioni generate dalle opzioni della piattaforma
+        i = this.platforms[platid].min_jump;
+        nextplatid = platid + i;
+        check = this.scout(nextplatid, check) || check;
+        i = this.platforms[platid].max_jump;
+        nextplatid = platid + i;
+        check = this.scout(nextplatid, check) || check;
+        if (check) {
+          return true;
+        }
+      }
+    }
+    return check;
+  };
+
+  playfieldconfirmer(min, max) {
+    if (this.scout(0, false)) {
+    } else {
+      this.createplayfield(min, max);
+      this.playfieldconfirmer(min, max);
+    }
+  };
+
+  changeGameMode(){
+    if(this.gameMode == 0){
+      this.gameMode = 1;
+    }else{
+      this.gameMode = 0;
+    };
+  };
+
+};
